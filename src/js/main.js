df217432fa16663b3616675eface34f6b8a5ed61
suraiyasultana826 <suraiyasultana826@gmail.com>
2024-09-06 19:56:59 +0600

    ..

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-
+import gsap from 'gsap';
 import * as THREE from 'three';
 import texture from '../img/images.jpg';
 import vertexShader from '../shaders/vertex.glsl';
@@ -31,8 +31,7 @@ const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
    
 }))
 
-console.log(sphere);
-scene.add(sphere);
+
 
 //create atmosphere
 const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
@@ -48,7 +47,38 @@ atmosphere.scale.set(1.4, 1.4, 1.4);
 
 scene.add(atmosphere);
 
+
+
+
+const group = new THREE.Group()
+group.add(sphere);
+scene.add(group);
+
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({
+    color: 0xffffff
+})
+
+const starVertices = []
+for( let i = 0; i< 10000; i++){
+    const x = (Math.random() - 0.5) * 2000
+    const y = (Math.random() - 0.5) * 2000
+    const z = -Math.random() * 2000
+    starVertices.push(x, y, z)
+}
+
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(
+    starVertices, 3
+))
+
+const stars = new THREE.Points(starGeometry, starMaterial)
+scene.add(stars)
+
 camera.position.z = 10;
+const mouse = {
+    x : undefined,
+    y: undefined
+}
 
 function animate() {
 
@@ -56,13 +86,14 @@ function animate() {
 
 	renderer.render( scene, camera );
     sphere.rotation.y += 0.0001;
+    gsap.to(group.rotation, {
+        x:-mouse.y * 0.3,
+        y: mouse.x * 0.5,
+        duration: 2
+    })
 
 }
 animate()
-const mouse = {
-    x : undefined,
-    y: undefined
-}
 
 addEventListener('mousemove', () => {
     mouse.x = (event.clientX / innerWidth) * 2 -1
